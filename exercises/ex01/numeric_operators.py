1989f3bad361599f8d380a31f6222943f651c835
Sophie Jiang <sjiang13@email.unc.edu>
2021-05-20 13:45:58 -0400

    ex1 (1)

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,16 +1,16 @@
-"""Numeric Operators Exercise"""
+"""Numeric Operators Exercise!"""
 
 __author__: str = "730411082"
 
 # Your solution starts here...
 left_side_str: str = input("What number is on the left hand side? ")
-#print(type(left_side_str))
+# print(type(left_side_str))
 right_side_str: str = input("What number is on the right hand side? ")
 left_int = int(left_side_str)
 right_int = int(right_side_str)
-#print(type(left_int))
-#multiply = left_int**right_int
-#print(multiply)
+# print(type(left_int))
+# multiply = left_int**right_int
+# print(multiply)
 print(left_side_str + " ** " + right_side_str + ' is ' + str(left_int**right_int))
 print(left_side_str + " / " + right_side_str + ' is ' + str(left_int/right_int))
 print(left_side_str + " // " + right_side_str + ' is ' + str(left_int//right_int))
