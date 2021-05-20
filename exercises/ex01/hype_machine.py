d476d412f12d51c55156ba61000fd7e16c4eb6b9
Sophie Jiang <sjiang13@email.unc.edu>
2021-05-20 13:38:00 -0400

    ex1 (0)

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,10 @@
-"""DESCRIPTION OF PROGRAM HERE"""
+"""Exercise 1 hype!"""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730411082"
 
 # Your solution starts here...
+name: str = input("What is your name? ")
+
+print(name + " is awesome!")
+print("You are amazing, " + name + '! :)')
+print("Hello " + name + ", I hope you have a fantastic day!")
