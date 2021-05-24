506483fd68d8db9c44a67130d401efc836ed26b5
Kaki Ryan <kakiryan6@gmail.com>
2021-05-23 21:57:04 -0400

    Starter code for week 1 exercises.

---
+++ exercises/ex04/factorial.py
@@ -0,0 +1,6 @@
+"""An exercise in computing the factorial of an int."""
+
+__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+
+
+# Begin your solution here...
---
+++ exercises/ex06/abbreviate.py
@@ -0,0 +1,16 @@
+"""A function to abbreviate strings."""
+
+__author__ = "YOUR 9-DIGIT PID"
+
+
+def main() -> None:
+    """The entrypoint of the program, when run as a module."""
+    # TODO 2: Prompt the user for text....
+    # .. and make use of abbreviate function and print it.
+
+
+# TODO 1: Define the abbreviate function, and its logic, here.
+
+
+if __name__ == "__main__":
+    main()
