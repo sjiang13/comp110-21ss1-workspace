a731905fedbe75f31396e013e50b7a83ee95f8aa
Sophie Jiang <sjiang13@email.unc.edu>
2021-05-26 22:45:41 -0400

    ex04 00

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -1,6 +1,14 @@
 """An exercise in computing the factorial of an int."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730411082"
 
 
 # Begin your solution here...
+input_int: int = int(input("Choose a number: "))
+factorial = 1
+int_to_multiply = 1 
+while int_to_multiply <= input_int: 
+    factorial = factorial * int_to_multiply
+    int_to_multiply += 1
+    # print(factorial)
+print(factorial)
--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -1,6 +1,26 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730411082"
 
 
 # Begin your solution here...
+input_beat: str = input("What beat do you want to repeat? ")
+
+input_times: int = int(input("How many times do you want to repeat it? "))
+input_spacing = " " + input_beat
+
+final_str = ""
+# print(final_str)
+# print(len(final_str))
+if input_times <= 0:
+    final_str = final_str + "No beat..."
+while input_times > 0:
+    final_str = final_str + input_beat
+    input_times -= 1
+    input_beat = input_spacing
+
+print(final_str)
+
+
+
+# print(final_str)
