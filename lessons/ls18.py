4aff312ad52969e66a8b57afeb829e91dd15ecaa
Sophie Jiang <sjiang13@email.unc.edu>
2021-06-03 17:42:00 -0400

    jun3

--- lessons/ls18.py
+++ lessons/ls18.py
@@ -27,3 +27,43 @@ def passes(students:dict[str, float]) -> list[str]:
 
 s: dict[str, float] = {"Andrew": 100.0, "Shaurik": 89.0, "Claire": 40.0}
 print(passes(s))
+
+teachers_pet: list[str] = ["Claire", "Jasper"]
+
+for pet in teachers_pet:
+    # initialize or increment
+    if pet in s:
+        # increment since the pet key is valid, and exists in the s dictionary
+        s[pet] += 50.0
+    else:
+        # initializing bc the key is not yet in the dictionary
+        s[pet] = 65.0
+
+frequency_table: dict[str, int] = {}
+# for each color in input dictionry
+# if the color is already in teh table, increment its value
+# if not, add it to the table with a starting value of 1
+
+print(s)
+
+a: list[int] = [1, 2, 3, 4, 5, 6]
+b: list[int] = [2, 4, 6]
+
+for e in a: 
+    if e not in b:
+        print(f"NO {e} is not in list b!")
+
+
+def max_algo(xs: list[int]) -> int:
+    # make the assumption that the list is not empty
+    result: int = xs[0]
+    for item in xs:
+        if item > result:
+            result = item
+    return result
+
+# steps for finding favorite color:
+# 1. make frequency table, looping over the input parameter (keys or values)
+# initialize vs increment pattern
+# 2. max color - perform the max algorithm
+# keep track of not only the curent max as we go, but also the color associated with the max
